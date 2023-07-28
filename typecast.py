b7e64732665522ad2bb36fd029f13a6b0a45247e
Arangarajan-Aero <arangarajanmoorthy002@gmail.com>
2023-07-28 18:24:04 +0530

    practice python

---
+++ typecast.py
@@ -0,0 +1,25 @@
+a=10.3
+b="12"
+c=9
+x=int(a)
+print(x)
+print(int(b))
+print(int(c))
+
+"""int type casting
+only support integer,
+float,string numbers"""
+
+#string
+print(str("3"))
+print(str("Aero"))
+print(str("3.7"))
+print(str("0+1j"))
+"""any number or words within
+double bracket can convert
+into string"""
+
+#float
+print(float("3"))#string to float
+print(float(3))#integer to float
+print(float(3.7))#float to float
