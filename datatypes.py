3f8be12b609f22d827344b278df505466721ee4a
Arangarajan-Aero <arangarajanmoorthy002@gmail.com>
2023-07-28 18:21:59 +0530

    python practice

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+#number
+n=1
+print("the number",n)
+#float
+f=1.3
+print("float number",f)
+#complex
+c=0+1j
+print("the complex",c)
+#string
+s="Aero"
+a="123"
+w="1.3"
+e="0+1j"
+print(s,a,w,e)
+#list
+l=[1,2,3,4,5]
+#mutable
+print(l)
+#set
+s=(a,5,1.0,0+3j,"hlo")#nonunique element
+print("set",s)
+#tuple
+t={100,200,3,4,}#unordered collection of unique element
+print("tuple",t)
+#dictionary
+d={"Name":"AerO","age":20}
+print("dictionary",d)
+
+
