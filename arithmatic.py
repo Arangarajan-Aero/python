21f2680e752f082d67b349855735384256498121
Arangarajan-Aero <arangarajanmoorthy002@gmail.com>
2023-07-28 18:28:55 +0530

    arithmatic operation file added

---
+++ arithmatic.py
@@ -0,0 +1,10 @@
+#arithmetic operation
+a=5
+b=10
+print(a+b)#addition
+print(a-b)#subtraction
+print(a*b)#multiplication
+print(a/b)#division it return quotient
+print(a//b)#floordivision it return quotent without decimal point
+print(a**b)#exponentioal 
+print(a%b)#modolus it return remainder
